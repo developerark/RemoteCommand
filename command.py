d5a21df9dfe3f0cdbf06e3cfe386053e56f5ffa8
Aswin Raj Kharel <aswin.kharel@bnsf.com>
2018-09-15 18:56:20 -0500

    command line interface changed

--- command.py
+++ command.py
@@ -2,17 +2,16 @@ import argparse
 import socket
 import json 
 
+# Parse Command Line Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("host", type=str, help="Host address to connect to")
 parser.add_argument("port", type=int, help="Service port number")
 parser.add_argument("command", type=str, help="Command to execute on remote machine")
 args = parser.parse_args()
 
+# Send the Payload
 service = socket.socket()
 service.connect((args.host, args.port))
-
 payload = {"command": args.command}
-
-service.send(json.dumps(payload))
-
+service.send(json.dumps(payload).encode())
 service.close()
