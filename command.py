ff893a64df1e4494798e86e30aa05c1c642b7923
Aswin Raj Kharel <aswin.kharel@bnsf.com>
2018-09-15 18:52:26 -0500

    Command line argument changed

--- command.py
+++ command.py
@@ -2,15 +2,14 @@ import argparse
 import socket
 import json 
 
-host = '127.0.0.1'
-port = 7100
-
 parser = argparse.ArgumentParser()
+parser.add_argument("host", type=str, help="Host address to connect to")
+parser.add_argument("port", type=int, help="Service port number")
 parser.add_argument("command", type=str, help="Command to execute on remote machine")
 args = parser.parse_args()
 
 service = socket.socket()
-service.connect((host, port))
+service.connect((args.host, args.port))
 
 payload = {"command": args.command}
 
